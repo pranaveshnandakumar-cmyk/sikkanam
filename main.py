c28ff68013b69b34fae77c4509ccad7ed18ab477
pranaveshnandakumar-cmyk <pranaveshnandakumar@gmail.com>
2026-03-24 15:34:30 +0530

    Implement CORS middleware in FastAPI app
    Added CORS middleware to allow cross-origin requests.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class Trip(BaseModel):
     source: str
     destination: str

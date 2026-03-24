e00cde7ae629752bc873a43e48bfbae0b965b7f2
pranaveshnandakumar-cmyk <pranaveshnandakumar@gmail.com>
2026-03-24 15:03:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Trip(BaseModel):
+    source: str
+    destination: str
+    days: int
+    budget: int
+
+@app.post("/plan-trip")
+def plan_trip(trip: Trip):
+
+    return {
+        "route": [
+            f"{trip.source} → Salem (Train)",
+            f"Salem → {trip.destination} (Bus)"
+        ],
+        "travel_cost": 500,
+        "hotel_budget_per_night": (trip.budget * 0.4) / trip.days,
+        "recommended_hotels": [
+            "Green Valley Stay",
+            "Hill View Lodge"
+        ],
+        "tourist_places": [
+            "Lake",
+            "View Point"
+        ]
+    }
